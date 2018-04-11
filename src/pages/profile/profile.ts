b9b9c8d34ac401d32d1f3850171a61a87aec6412
Vidovitsch <david.de.bekker@gmail.com>
2018-04-11 22:28:38 +0200

    Kweet fetching when kweet-segment is selected

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -22,10 +22,17 @@ export class ProfilePage {
 
   profileObservable: Observable<any>;
   userNumbersObservable: Observable<any>;
+  followingObservable: Observable<any>;
+  followersObservable: Observable<any>;
+  kweetsObservable: Observable<any>;
 
   username: string;
   userNumbers: any;
   profile: any;
+
+  following: any;
+  followers: any;
+  kweets: any;
 
 
   constructor (
@@ -43,6 +50,30 @@ export class ProfilePage {
 
     this.fetchProfile(this.username)
     this.fetchUserNumbers(this.username)
+  }
+
+  showFollowing() {
+    this.followingObservable = this.httpClient.get(this.singletonService.getFollowingCall(this.username));
+    this.followingObservable.subscribe(data => {
+      console.log(data);
+      this.following = data;
+    });
+  }
+
+  showFollowers() {
+    this.followersObservable = this.httpClient.get(this.singletonService.getFollowersCall(this.username));
+    this.followersObservable.subscribe(data => {
+      console.log(data);
+      this.followers = data;
+    });
+  }
+
+  showKweets() {
+    this.kweetsObservable = this.httpClient.get(this.singletonService.searchKweetsCall(this.username));
+    this.kweetsObservable.subscribe(data => {
+      console.log(data);
+      this.kweets = data;
+    });
   }
 
   fetchProfile(username) {
